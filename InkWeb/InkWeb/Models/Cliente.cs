700d78e903cb4f31d65debae5f528c666bdf004f
jorelrx <49566557+jorelrx@users.noreply.github.com>
2021-02-28 15:51:33 -0300

    SO vai

---
+++ InkWeb/InkWeb/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InkWeb.Models
+{
+    public class Cliente
+    {
+        public long ClienteId { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Numero_Tell { get; set; }
+        public string Instagram { get; set; }
+        public string Data_Nascimento { get; set; }
+        public string Sexo { get; set; }
+    }
+}
