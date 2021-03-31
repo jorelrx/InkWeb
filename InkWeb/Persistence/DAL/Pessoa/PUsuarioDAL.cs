2441856a2761bf0b424b8610825742d5370e5643
jorelrx <49566557+jorelrx@users.noreply.github.com>
2021-03-31 04:26:38 -0300

    upando arquivos atualizados

---
+++ InkWeb/Persistence/DAL/Pessoa/PUsuarioDAL.cs
@@ -0,0 +1,36 @@
+﻿using Models.Pessoa;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.DAL
+{
+    public class PUsuarioDAL
+    {
+        private EFContext context = new EFContext();
+
+        //Listar Usuarios
+        public IQueryable<MUsuario> ListarUsuarios()
+        {
+            return context.Usuarios.OrderBy(u => u.Nome);
+        }
+
+        //Adicionar usuarios e editar dados
+        public void SaveUsuario(MUsuario mUsuario)
+        {
+            if (mUsuario.Id == 0)
+            {
+                context.Usuarios.Add(mUsuario);
+            }
+            else
+            {
+                context.Entry(mUsuario).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ InkWeb/Services/Pessoa/SUsuario.cs
@@ -0,0 +1,23 @@
+﻿using Models.Pessoa;
+using Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Pessoa
+{
+    public class SUsuario
+    {
+        private PUsuarioDAL usuarioDAL = new PUsuarioDAL();
+        public IQueryable<MUsuario> ListarUsuarios()
+        {
+            return usuarioDAL.ListarUsuarios();
+        }
+        public void SaveUsuario(MUsuario mUsuario)
+        {
+            usuarioDAL.SaveUsuario(mUsuario);
+        }
+    }
+}
